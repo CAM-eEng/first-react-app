55ba1a3c536ba864887eb1cb3419b77fd2dfde27
Cameron <camm.hartmann@gmail.com>
2020-04-26 21:01:40 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header' 
+import Footer from './components/footer'
+
+function createAlert(){
+  alert('Hello. You clicked me!');
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const userlogin = true;
+  if (userlogin) {
+    return (
+      <div className="App">
+        <Header info="this is my message" />
+        <Footer trademark="page by Cameron Hartman"
+          myAlert={createAlert}/>
+      </div>
+    );
+  } else {
+    return <h2>Forbidden</h2>
+  }
 }
 
 export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { CtxConsumer } from '../index';
+
+/* eslint-disable */
+
+class Footer extends Component {
+
+    state = {
+        name: 'Cameron',
+        age: 0,
+        isLogin: true
+    }
+
+    componentDidMount(){
+        this.setState({name: 'MyName'});
+        // subscribe
+    }
+
+    changed = evt => {
+        this.setState({name: evt.target.value});
+        console.log(this.state.name);  
+    }
+
+    render() {
+        return (
+            <CtxConsumer>
+                {context => {
+                    <div>
+                        {context.animals && context.animals.map(animal => {
+                            return (
+                                <div key={animal}>
+                                    <h1>{animal}</h1>
+                                </div>
+                            );
+                        }) }
+                    </div>
+                }}
+            </CtxConsumer>    
+        );
+    }
+}
+
+export default Footer;
